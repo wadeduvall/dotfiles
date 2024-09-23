7fcb6a063c98e0711b5a7dcfd8ed17f1fbaf9407
Wade Duvall <wsduvall@proton.me>
2024-09-23 06:56:10 -0400

    chore: update typescript language server name

--- dot_config/nvim/lua/utils/init.lua
+++ dot_config/nvim/lua/utils/init.lua
@@ -1,39 +1,39 @@
 local M = {}
 
 M.servers = {
-  "lua_ls",
-  "cssls",
-  "html",
-  "tsserver",
-  "pyright",
-  "bashls",
-  "jsonls",
-  "yamlls",
-  "clangd",
-  "taplo",
-  "marksman",
-  "cmake",
-  "rust_analyzer",
-  -- "ruff_lsp",
-  -- "ltex",
+	"lua_ls",
+	"cssls",
+	"html",
+	"ts_ls",
+	"pyright",
+	"bashls",
+	"jsonls",
+	"yamlls",
+	"clangd",
+	"taplo",
+	"marksman",
+	"cmake",
+	"rust_analyzer",
+	-- "ruff_lsp",
+	-- "ltex",
 }
 
 M.linters = {
-  "clang-format",
-  "flake8",
-  "prettier",
-  "stylua",
-  "black",
-  "isort",
-  "shfmt",
-  -- "codespell",
-  -- "vale",
+	"clang-format",
+	"flake8",
+	"prettier",
+	"stylua",
+	"black",
+	"isort",
+	"shfmt",
+	-- "codespell",
+	-- "vale",
 }
 
 M.daps = {
-  "python",
-  "codelldb",
-  "js",
+	"python",
+	"codelldb",
+	"js",
 }
 
 return M
